3563ec6861a5b56e626d1185b90b90853544128f
CaptainKenji17 <kenjitheextreme@gmail.com>
2015-10-01 03:50:24 +0900

    Plugin

---
+++ src/Info/Main.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Info;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player;
+use pocketmine\utils\TextFormat;
+use pocketmine\level\sound\ClickSound;
+
+class Main extends PluginBase {
+    
+    public function onEnable() {
+    $this->getLogger()->info(TextFormat::GREEN . "Plugin Enabled!");
+    }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        if($sender instanceof $player) {
+            $name = $sender->getName();
+            if(strtolower($command->getName()) === 'info') {
+                if(count($args) < 1) {
+                   $level = $sender->getLevel();
+                   $level->addSound(new ClickSound($sender->getLocation()));
+                   $sender->sendMessage("Info \n1.Test\n2. Test2");
+                   return;
+                } else {
+                      $sender->sendMessage("this command requires nothing");
+                     return;
+                }
+            }
+        }
+        $sender->sendMessage("You must run this cmd on game not on console bb");
+        return;
+    }
+}
+
